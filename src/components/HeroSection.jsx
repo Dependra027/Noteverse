5de35df1d2f16dcde84cc47c3d6a7656a6645d70
Dependra <dependrasingh027@gmail.com>
2025-01-05 12:56:55 +0530

    updated pictures

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,23 +11,23 @@ const HeroSection = () => {
     {
       title: "School Notes",
       description: "Comprehensive notes covering Classes 10th, 11th, 12th.",
-      image: "/src/assets/download.jpg",
+      image: "https://images.pexels.com/photos/247899/pexels-photo-247899.jpeg",
     },
     {
       title: "College Notes",
       description: "Detailed notes on Every Subjects OF College.",
-      image: "/src/assets/o.jpg",
+      image: "https://images.pexels.com/photos/207700/pexels-photo-207700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     
     },
     {
       title: "DEFENCE Related Notes",
       description: "In-depth notes For Defence Exams Like Nda, Cds, and many more...",
-      image: "/src/assets/downloadb.jpg",
+      image: "https://i.pinimg.com/736x/7b/e8/0e/7be80eb7972fcbb7544b46e658e24143.jpg",
     },
     {
       title: "BTech CSE Notes",
       description: "Notes covering algorithms and data structures.",
-      image: "/src/assets/downloada.jpg",
+      image: "https://i.pinimg.com/736x/d7/7f/a3/d77fa3f7ef6d2458a8ba82053362c5f1.jpg",
     },
   ];
   
